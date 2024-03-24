159ac79bf7473a30b8b25b3a1357abd71be9ad81
Alexey Ageev <literzap@yandex.ru>
2024-03-24 22:31:20 +0300

    fix DEBUG #5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
-DEBUG = str(os.getenv('DEBUG')).lower() == 'true'
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
